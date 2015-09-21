fca4371813b5edff67523ee3cdbc1c9f488ce1cf
huanglifu <huanlifu@gmail.com>
2015-09-21 20:33:01 +0800

    login icon

--- seller/src/main/java/com/huotu/huobanmall/seller/activity/MoreStatisticActivity.java
+++ seller/src/main/java/com/huotu/huobanmall/seller/activity/MoreStatisticActivity.java
@@ -1,22 +1,16 @@
 package com.huotu.huobanmall.seller.activity;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.huotu.huobanmall.seller.R;
-import com.huotu.huobanmall.seller.bean.BaseModel;
 import com.huotu.huobanmall.seller.bean.MJOtherStatisticModel;
-import com.huotu.huobanmall.seller.bean.OtherStatisticModel;
 import com.huotu.huobanmall.seller.common.Constant;
 import com.huotu.huobanmall.seller.utils.ActivityUtils;
 import com.huotu.huobanmall.seller.utils.DialogUtils;
